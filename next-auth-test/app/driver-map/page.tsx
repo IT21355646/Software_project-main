d15addbba7f5d59a1919c2dbe9a8c1383b3ff415
Sithumi <it21355646@my.sliit.lk>
2024-10-22 11:18:23 +0530

    Modifications

--- next-auth-test/app/driver-map/page.tsx
+++ next-auth-test/app/driver-map/page.tsx
@@ -8,13 +8,15 @@ import DriverMapComponent from "../../components/DriverMapComponent";
 export default function DriverDashboard() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const state = "driver";
 
   useEffect(() => {
     if (status === "unauthenticated") {
       router.push("/login");
-    } else if (session?.user?.role !== "driver") {
-      router.push("/unauthorized");
-    }
+    } 
+    // else if (session?.user?.role !== "driver") {
+    //   router.push("/unauthorized");
+    // }
   }, [session, status, router]);
 
   console.log("session >>> ", session);
@@ -24,7 +26,7 @@ export default function DriverDashboard() {
   }
 
   if (session?.user?.role === "driver") {
-    return <DriverMapComponent />;
+      return <DriverMapComponent role={session?.user?.role} state={state}/>;
   }
 
   return null;
